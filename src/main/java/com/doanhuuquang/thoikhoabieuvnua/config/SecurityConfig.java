8de76944546d89ebd093ba25899d32c09f1ba672
Doan Huu Quang <doanhuuquang.dev@gmail.com>
2025-06-01 16:21:42 +0700

    Fix bug

--- src/main/java/com/doanhuuquang/thoikhoabieuvnua/config/SecurityConfig.java
+++ src/main/java/com/doanhuuquang/thoikhoabieuvnua/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.doanhuuquang.thoikhoabieuvnua.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,10 +19,15 @@ public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.requestMatchers("/api/**").permitAll()
-				.anyRequest().authenticated()
-		).httpBasic(withDefaults());
-
-		return http.build();
+	    http
+	        .csrf(csrf -> csrf.disable())
+	        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers("/api/**").permitAll()
+	            .anyRequest().authenticated()
+	        )
+	        .httpBasic(withDefaults());
+	    
+	    return http.build();
 	}
 }
